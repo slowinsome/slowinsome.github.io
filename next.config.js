cc77471ec72f2205a214619b7fcd4975eea5f71e
slowinsome <slowinsome@gmail.com>
2022-10-08 01:33:19 +0900

    Add assetPrefix for Github Page
    Referenced from:
    https://wallis.dev/blog/next-js-basepath-and-assetprefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
-  }
+  },
+  assetPrefix: '/slowinsome.github.io/', // assetPrefix requires the trailing slash
 }
 
 module.exports = nextConfig
