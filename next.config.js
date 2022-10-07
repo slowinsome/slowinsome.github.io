fd2cdfbf20fbfd0d176ec8b2e33065109696b61e
slowinsome <slowinsome@gmail.com>
2022-10-08 01:36:09 +0900

    Revert "Add assetPrefix for Github Page"
    This reverts commit cc77471ec72f2205a214619b7fcd4975eea5f71e.

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
-  },
-  assetPrefix: '/slowinsome.github.io/', // assetPrefix requires the trailing slash
+  }
 }
 
 module.exports = nextConfig
